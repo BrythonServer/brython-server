9c05da65052808e58ecb95af19498ca7919c2b0a
Eric Dennison <ericd@netdenizen.com>
2015-03-26 14:33:24 +0000

    Updating with working files

--- main.py
+++ main.py
@@ -24,7 +24,11 @@ def tempdir():
 
 @app.route('/')
 def root():
-  return render_template('index.html', main=None)
+    return render_template('index.html', main=None)
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico') 
 
 @app.route('/<filename>')
 def file(filename):
