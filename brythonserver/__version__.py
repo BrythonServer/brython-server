a9fc4dcb3bdff276c32e6faa366bbad6d01aa615
ericd <ericd@netdenizen.com>
2019-07-26 12:57:56 -0400

    2.1.5 - smaller drive icon image file

--- brythonserver/__version__.py
+++ brythonserver/__version__.py
@@ -3,4 +3,4 @@ Brython-Server version definition.
 Author: E Dennison
 """
 
-VERSION = "2.1.4"
+VERSION = "2.1.5"
--- scripts/buildrelease.sh
+++ scripts/buildrelease.sh
@@ -4,6 +4,7 @@ mkdir -p brythonserver/static/brython
 pushd brythonserver/static/brython
 python3 -m brython --update
 popd
+rm dist/*
 python3 setup.py sdist
 python3 -m pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
 
