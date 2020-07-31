6c9a6f38ddc639e42a8691c6e75b946285f6a519
ericd <ericd@netdenizen.com>
2020-07-31 14:48:39 -0400

    add test for javascript coding style

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 black --check brythonserver || { echo 'black failed (use black first)' ; exit 1; }
 python3 -m pylint -r n brythonserver || { echo 'pylint failed' ; exit 1; }
+standard brythonserver/static/bs.js || { echo 'javascript lint failed (standard)' ; exit 1;}
