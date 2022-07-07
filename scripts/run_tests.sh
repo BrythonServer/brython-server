db9377ed746d6a72d9b80da1727a83898f7781c0
Eric Dennison <ericd@netdenizen.com>
2022-07-07 01:14:34 +0000

    more github workflow

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 -m black --check brythonserver || { echo 'black failed (use black first)' ; exit 1; }
+source env/bin/activate
+black --check brythonserver || { echo 'black failed (use black first)' ; exit 1; }
 python3 -m pylint -r n brythonserver || { echo 'pylint failed' ; exit 1; }
